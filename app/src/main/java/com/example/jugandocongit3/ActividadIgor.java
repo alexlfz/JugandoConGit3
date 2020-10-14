77c06e0070495741f4667c5326654ab2a17cd8bd
igor.krnyk@gmail.com <Ciberom123>
2020-10-14 16:31:17 +0200

    Commit actividad Igor

---
+++ app/src/main/java/com/example/jugandocongit3/ActividadIgor.java
@@ -0,0 +1,14 @@
+package com.example.jugandocongit3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadIgor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_igor);
+    }
+}
